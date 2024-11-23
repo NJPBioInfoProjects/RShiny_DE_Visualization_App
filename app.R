6e0ff8f0633bd6b558fea4eafd2c8902790abb6c
Nicolas Petrunich <nicolaspetrunich@Nicolass-MacBook-Pro.local>
2024-11-23 12:20:35 -0500

    test

--- app.R
+++ app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Nick Petrunich BF591 Final Project"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
